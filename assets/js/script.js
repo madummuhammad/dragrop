1f12294ec7eb10dea7d4f121761181762a315b6f
Muhammad Ma'dum <muhammad.madum2018@gmail.com>
2022-07-24 01:38:35 +0700

    add login 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,49 @@
-$(document).ready(function(){
+  AOS.init();
+  $(document).ready(function(){
 
-  var accItem = $('.sidebar-right .nav-item');
-  var accHD = $("[data-toggle=sidebar-right]");
+    var accItem = $('.sidebar-right .nav-item');
+    var accHD = $("[data-toggle=sidebar-right]");
 
-  for (i = 0; i < accHD.length; i++) {
-    accHD[i].addEventListener('click', toggleItem, false);
-  }
-
-  function toggleItem() {
-    var target = $(this).data('target');
-    var itemClass = $(target).parent().attr('class');
-    for (i = 0; i < accItem.length; i++) {
-      accItem[i].className = 'nav-item close';
-    }
-    if (itemClass == 'nav-item close') {
-      $(target).parent().addClass('nav-item open');
+    for (i = 0; i < accHD.length; i++) {
+      accHD[i].addEventListener('click', toggleItem, false);
     }
 
-  }
-
-  $('.owl-carousel').owlCarousel({
-    autoWidth:true,
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-      0:{
-        items:1
-      },
-      600:{
-        items:2
-      },
-      1000:{
-        items:2
+    function toggleItem() {
+      var target = $(this).data('target');
+      var itemClass = $(target).parent().attr('class');
+      for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'nav-item close';
       }
+      if (itemClass == 'nav-item close') {
+        $(target).parent().addClass('nav-item open');
+      }
+
     }
-  })
 
-  var myModal = new bootstrap.Modal(document.getElementById('login'), {
-    keyboard: false
-  })
+    $('.owl-carousel').owlCarousel({
+      autoWidth:true,
+      loop:true,
+      margin:10,
+      nav:false,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        1000:{
+          items:2
+        }
+      }
+    })
 
-  myModal.show()
-});
+    var myModal = new bootstrap.Modal(document.getElementById('login'), {
+      keyboard: false
+    })
+
+    myModal.show()
+  });
 
 
 
