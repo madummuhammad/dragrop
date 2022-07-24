e3fd84ae94f905331d9f33dacf016a5c2608c2ce
Muhammad Ma'dum <muhammad.madum2018@gmail.com>
2022-07-24 19:15:15 +0700

    kurang versi mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
 
     var accItem = $('.sidebar-right .nav-item');
-    var accHD = $("[data-toggle=sidebar-right]");
+    var accHD = $("[data-toggle=sidebar-right].active");
 
     for (i = 0; i < accHD.length; i++) {
       accHD[i].addEventListener('click', toggleItem, false);
@@ -17,6 +17,27 @@
       if (itemClass == 'nav-item close') {
         $(target).parent().addClass('nav-item open');
       }
+
+    }
+
+
+    var accItem2 = $('.sidebar-right .nav-item');
+    var accHD2 = $("[data-toggle=sidebar-right]:not(.active)");
+
+    for (i = 0; i < accHD2.length; i++) {
+      accHD2[i].addEventListener('click', toggleItem2, false);
+    }
+
+    function toggleItem2() {
+      // var target = $(this).data('target');
+      // var itemClass2 = $(target).parent().attr('class');
+      // for (i = 0; i < accItem2.length; i++) {
+      //   accItem2[i].className = 'nav-item close';
+      // }
+      // if (itemClass2 == 'nav-item close') {
+      //   $(target).parent().addClass('nav-item open');
+      // }
+      alert('adfasdf');
 
     }
 
